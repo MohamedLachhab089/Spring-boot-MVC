a19af1ff00e534271f6601b75b226acc99ac2a95
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-03-31 16:54:26 +0000

    last commit

--- src/main/java/ma/emsi/studentmvc/entites/Student.java
+++ src/main/java/ma/emsi/studentmvc/entites/Student.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Student {
+public class Student { // comment
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
